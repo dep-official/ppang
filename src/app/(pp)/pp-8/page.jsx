7f5a71af81c2bd72005b7ca12d04fa900056273a
dep-official <github@dep.team>
2026-01-02 20:57:22 +0900

    feat: PP-8 시술 페이지 이미지 에셋 추가

---
+++ src/app/(pp)/pp-8/page.jsx
@@ -0,0 +1,10 @@
+import ImageSequence from "@/components/ImageSequence/ImageSequence";
+
+const pp8page = () => {
+  // Currently using the single image from before as a placeholder
+  return (
+    <ImageSequence dirPath="/pages/pp-8" images={["001.png","002.png","003.png","004.png","005.png","006.png","007.png","008.png"]} />
+  )
+}
+
+export default pp8page
